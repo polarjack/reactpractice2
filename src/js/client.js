54d863e9c3e6d7e477fa521fd0bd0f77aebd7e6a
POLARGG <luckyjack85@gmail.com>
2017-03-06 13:06:20 +0800

    youtube 8

---
+++ src/js/client.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Router, Route, IndexRoute, hashHistory } from "react-router";
+
+import Bootstrap from "./vender/bootstrap-without-jquery.min.js";
+
+import Layout from "./pages/Layout";
+import Featured from "./pages/Featured";
+import Settings from "./pages/Settings";
+import Archives from "./pages/Archives";
+
+const app = document.getElementById('app');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}> </IndexRoute>
+      <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+      <Route path="settings" component={Settings}></Route>
+    </Route>
+  </Router>,
+app);
---
+++ src/js/pages/Layout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router";
+
+export default class Layout extends React.Component {
+  navigate() {
+    this.props.history.replaceState(null, "/");
+  }
+  render() {
+    const {history} = this.props;
+    console.log(history.isActive("archives"));
+    return (
+      <div>
+        <h1> KillerNews.net</h1>
+        {this.props.children}
+        <Link to="archives" activeClassName="test"><button class="btn btn-success">archives</button></Link>
+        <Link to="settings" class="btn btn-primary">settings</Link>
+        <button class="btn btn-danger" onClick={this.navigate.bind(this)}>Featured</button>
+    </div>
+    );
+  }
+}
