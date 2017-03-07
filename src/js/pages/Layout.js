538f0a69226017e0dcaf6816ce7fc78838efbf72
Jack Yu <luckyjack85@gmail.com>
2017-03-07 17:43:42 +0800

    8 half

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -2,19 +2,23 @@ import React from "react";
 import { Link } from "react-router";
 
 export default class Layout extends React.Component {
-  navigate() {
-    this.props.history.replaceState(null, "/");
-  }
   render() {
-    const {history} = this.props;
-    console.log(history.isActive("archives"));
+    const { location }  = this.props;
+    const containerStyle = {
+      marginTop: "60px"
+    };
     return (
       <div>
-        <h1> KillerNews.net</h1>
-        {this.props.children}
-        <Link to="archives" activeClassName="test"><button class="btn btn-success">archives</button></Link>
-        <Link to="settings" class="btn btn-primary">settings</Link>
-        <button class="btn btn-danger" onClick={this.navigate.bind(this)}>Featured</button>
+        <Nav location={location}/>
+        <div class="container" style={containerStyle}>
+          <div class="row">
+            <div class="col-lg-12">
+              <h1>KillerNews.net</h1>
+              {this.props.children}
+            </div>
+          </div>
+          <Footer/>
+        </div>
       </div>
     );
   }
